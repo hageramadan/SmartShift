15f46737d048d06fa6a849d9e91e83571fc7fce9
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-11-19 11:18:07 +0200

    integration between location and database

--- src/app/pages/locations/locations.ts
+++ src/app/pages/locations/locations.ts
@@ -1,79 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocationI } from '../../models/location-i';
+import { DepartmentI } from '../../models/department-i';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { DepartmentI } from '../../models/department-i';
+import { CrudService } from '../../services/crud.service';
+import { SharedService } from '../../services/shared.service';
+
 @Component({
   selector: 'app-locations',
-  imports: [CommonModule , FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './locations.html',
   styleUrl: './locations.css',
 })
-export class Locations {
-  locations: LocationI[] = [
-    // {
-    //   id: 1,
-    //   name: 'Main Hospital Building - Wing A',
-    //   address: '123 Medical Center Drive, Floor 1',
-    //   department: 'Emergency Department',
-    // },
-    // {
-    //   id: 2,
-    //   name: 'Main Hospital Building - Wing B',
-    //   address: '123 Medical Center Drive, Floor 3',
-    //   department: 'Intensive Care Unit',
-    // },
-    // {
-    //   id: 3,
-    //   name: 'Surgery Center',
-    //   address: '456 Healthcare Blvd',
-    //   department: 'Surgery Department',
-    // },
-  ];
- departments: DepartmentI[] = [
-
-    // // your department data here
-    //   {
-    //       id: 1,
-    //       name: 'Emergency Department',
-    //       description: 'Handles all emergency and urgent care patients',
-    //       manager: 'Dr. Michael Chen',
-    //       staffCount: 4,
-    //     },
-    //     {
-    //       id: 2,
-    //       name: 'Intensive Care Unit',
-    //       description: 'Critical care for severely ill patients',
-    //       manager: 'Dr. James Wilson',
-    //       staffCount: 2,
-    //     },
-    //     {
-    //       id: 3,
-    //       name: 'Surgery Department',
-    //       description: 'Surgical procedures and post-operative care',
-    //       manager: 'Dr. Sarah Johnson',
-    //       staffCount: 0,
-    //     },
-  ];
-  // newLocation: LocationI = { id: 0, name: '', address: '', department: '' };
+export class Locations implements OnInit {
+  locations: LocationI[] = [];
+  departments: DepartmentI[] = [];
+  
+  newLocation: LocationI = {
+    id: 0,
+    name: '',
+    street: '',
+    city: '',
+    state: '',
+    country: 'Egypt',
+    postalCode: ''
+  };
+  
   isModalOpen = false;
   isEditing = false;
   editId: number | null = null;
   showDeleteConfirm = false;
   locationToDelete: number | null = null;
+  isLoading = false;
 
-  constructor(private toastr: ToastrService) {}
+  constructor(
+    private toastr: ToastrService,
+    private crudService: CrudService,
+    private sharedService: SharedService
+  ) {}
+
+  ngOnInit() {
+    this.loadLocations();
+    this.loadDepartments();
+  }
+
+  loadLocations() {
+    this.isLoading = true;
+    this.crudService.getAll('locations').subscribe({
+      next: (res: any) => {
+        this.locations = res.data || res;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error loading locations:', err);
+        this.toastr.error('Error loading locations!', 'Error');
+        this.isLoading = false;
+      }
+    });
+  }
+
+  loadDepartments() {
+    this.sharedService.getDepartments().subscribe({
+      next: (departments) => {
+        this.departments = departments;
+      },
+      error: (err) => {
+        console.error('Error loading departments:', err);
+      }
+    });
+  }
 
   openModal(editLocation?: LocationI) {
     this.isModalOpen = true;
     if (editLocation) {
-      // this.newLocation = { ...editLocation };
+      this.newLocation = { ...editLocation };
       this.isEditing = true;
       this.editId = editLocation.id;
     } else {
       this.isEditing = false;
-      // this.newLocation = { id: 0, name: '', address: '', department: '' };
+      this.editId = null;
+      this.newLocation = {
+        id: 0,
+        name: '',
+        street: '',
+        city: '',
+        state: '',
+        country: 'Egypt',
+        postalCode: ''
+      };
     }
   }
 
@@ -81,32 +96,73 @@ export class Locations {
     this.isModalOpen = false;
     this.isEditing = false;
     this.editId = null;
-    // this.newLocation = { id: 0, name: '', address: '', department: '' };
+    this.newLocation = {
+      id: 0,
+      name: '',
+      street: '',
+      city: '',
+      state: '',
+      country: 'Egypt',
+      postalCode: ''
+    };
   }
 
   validateForm(): boolean {
-    // if (!this.newLocation.name || !this.newLocation.address || !this.newLocation.department) {
-    //   this.toastr.warning('Please fill in all fields!', 'Validation');
-    //   return false;
-    // }
+    if (!this.newLocation.name || 
+        !this.newLocation.street || 
+        !this.newLocation.city || 
+        !this.newLocation.state || 
+        !this.newLocation.country) {
+      this.toastr.warning('Please fill in all required fields!', 'Validation');
+      return false;
+    }
     return true;
   }
 
   saveLocation() {
     if (!this.validateForm()) return;
 
-    if (this.isEditing) {
-      const index = this.locations.findIndex((l) => l.id === this.editId);
-      if (index > -1) {
-        // this.locations[index] = { ...this.newLocation };
-        this.toastr.info('Location updated successfully!', 'Updated');
-      }
+    this.isLoading = true;
+    const locationData = {
+      name: this.newLocation.name,
+      street: this.newLocation.street,
+      city: this.newLocation.city,
+      state: this.newLocation.state,
+      country: this.newLocation.country,
+      postalCode: this.newLocation.postalCode || ''
+    };
+
+    if (this.isEditing && this.editId) {
+      // Update location
+      this.crudService.update('locations', this.editId.toString(), locationData).subscribe({
+        next: (res: any) => {
+          this.toastr.success('Location updated successfully!', 'Updated');
+          this.loadLocations();
+          this.closeModal();
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Error updating location:', err);
+          this.toastr.error('Error updating location!', 'Error');
+          this.isLoading = false;
+        }
+      });
     } else {
-      // this.newLocation.id = Date.now();
-      // this.locations.push({ ...this.newLocation });
-      this.toastr.success('New location added!', 'Created');
+      // Create new location
+      this.crudService.create('locations', locationData).subscribe({
+        next: (res: any) => {
+          this.toastr.success('New location added successfully!', 'Created');
+          this.loadLocations();
+          this.closeModal();
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Error adding location:', err);
+          this.toastr.error('Error adding location!', 'Error');
+          this.isLoading = false;
+        }
+      });
     }
-    this.closeModal();
   }
 
   confirmDelete(id: number) {
@@ -115,16 +171,40 @@ export class Locations {
   }
 
   deleteLocation() {
-    if (this.locationToDelete != null) {
-      this.locations = this.locations.filter((l) => l.id !== this.locationToDelete);
-      this.toastr.error('Location deleted successfully!', 'Deleted');
-      this.showDeleteConfirm = false;
-      this.locationToDelete = null;
+    if (this.locationToDelete !== null) {
+      this.isLoading = true;
+      this.crudService.delete('locations', this.locationToDelete.toString()).subscribe({
+        next: (res: any) => {
+          this.toastr.success('Location deleted successfully!', 'Deleted');
+          this.loadLocations();
+          this.showDeleteConfirm = false;
+          this.locationToDelete = null;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Error deleting location:', err);
+          this.toastr.error('Error deleting location!', 'Error');
+          this.isLoading = false;
+        }
+      });
     }
   }
 
   cancelDelete() {
     this.showDeleteConfirm = false;
     this.locationToDelete = null;
+  }
+
+  // Helper function to format address for display
+  getFullAddress(location: LocationI): string {
+    const parts = [
+      location.street,
+      location.city,
+      location.state,
+      location.postalCode,
+      location.country
+    ].filter(part => part && part.trim() !== '');
+    
+    return parts.join(', ');
   }
 }
--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -7,6 +7,9 @@ import { UserI } from '../models/user-i';
 import { DepartmentI } from '../models/department-i';
 import { PositionI } from '../models/position-i';
 import { LevelI } from '../models/level-i';
+import { LocationI } from '../models/location-i';
+import { SubDepartmentI } from '../models/sub-department-i';
+import { ShiftI } from '../models/shift-i';
 import { ApiResponse } from '../models/api-response';
 
 type Meta = {
@@ -15,6 +18,7 @@ type Meta = {
   page?: number;
   limit?: number
 };
+
 @Injectable({
   providedIn: 'root',
 })
@@ -23,20 +27,25 @@ export class SharedService {
   private departments$ = new BehaviorSubject<DepartmentI[]>([]);
   private positions$ = new BehaviorSubject<PositionI[]>([]);
   private levels$ = new BehaviorSubject<LevelI[]>([]);
+  private locations$ = new BehaviorSubject<LocationI[]>([]);
+  private subDepartments$ = new BehaviorSubject<SubDepartmentI[]>([]);
+  private shifts$ = new BehaviorSubject<ShiftI[]>([]);
 
   private meta = {
     users: new BehaviorSubject<Meta>({}),
     departments: new BehaviorSubject<Meta>({}),
     positions: new BehaviorSubject<Meta>({}),
     levels: new BehaviorSubject<Meta>({}),
+    locations: new BehaviorSubject<Meta>({}),
+    subDepartments: new BehaviorSubject<Meta>({}),
+    shifts: new BehaviorSubject<Meta>({}),
   };
 
-  private isLoaded = false; // Flag to track if data is loaded
+  private isLoaded = false;
 
   constructor(
     private toastr: ToastrService,
     private crud: CrudService,
-
   ) { }
 
   loadAll(): void {
@@ -47,17 +56,26 @@ export class SharedService {
       depts: this.crud.getAll<DepartmentI>('departments'),
       poss: this.crud.getAll<PositionI>('positions'),
       lvls: this.crud.getAll<LevelI>('levels'),
+      locs: this.crud.getAll<LocationI>('locations'),
+      subs: this.crud.getAll<SubDepartmentI>('sub-departments'),
+      shifts: this.crud.getAll<ShiftI>('shifts'),
     }).subscribe({
-      next: ({ users, depts, poss, lvls }) => {
+      next: ({ users, depts, poss, lvls, locs, subs, shifts }) => {
         this.users$.next(users?.data ?? []);
         this.departments$.next(depts?.data ?? []);
         this.positions$.next(poss?.data ?? []);
         this.levels$.next(lvls?.data ?? []);
+        this.locations$.next(locs?.data ?? []);
+        this.subDepartments$.next(subs?.data ?? []);
+        this.shifts$.next(shifts?.data ?? []);
 
         this.meta.users.next(this.extractMeta(users));
         this.meta.departments.next(this.extractMeta(depts));
         this.meta.positions.next(this.extractMeta(poss));
         this.meta.levels.next(this.extractMeta(lvls));
+        this.meta.locations.next(this.extractMeta(locs));
+        this.meta.subDepartments.next(this.extractMeta(subs));
+        this.meta.shifts.next(this.extractMeta(shifts));
 
         this.isLoaded = true;
       },
@@ -69,11 +87,17 @@ export class SharedService {
   getDepartments(): Observable<DepartmentI[]> { return this.departments$.asObservable(); }
   getPositions(): Observable<PositionI[]> { return this.positions$.asObservable(); }
   getLevels(): Observable<LevelI[]> { return this.levels$.asObservable(); }
+  getLocations(): Observable<LocationI[]> { return this.locations$.asObservable(); }
+  getSubDepartments(): Observable<SubDepartmentI[]> { return this.subDepartments$.asObservable(); }
+  getShifts(): Observable<ShiftI[]> { return this.shifts$.asObservable(); }
 
   getUsersMeta(): Observable<Meta> { return this.meta.users.asObservable(); }
   getDepartmentsMeta(): Observable<Meta> { return this.meta.departments.asObservable(); }
   getPositionsMeta(): Observable<Meta> { return this.meta.positions.asObservable(); }
   getLevelsMeta(): Observable<Meta> { return this.meta.levels.asObservable(); }
+  getLocationsMeta(): Observable<Meta> { return this.meta.locations.asObservable(); }
+  getSubDepartmentsMeta(): Observable<Meta> { return this.meta.subDepartments.asObservable(); }
+  getShiftsMeta(): Observable<Meta> { return this.meta.shifts.asObservable(); }
 
   refetchAll(): void {
     this.isLoaded = false;
